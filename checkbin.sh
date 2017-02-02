5c4395ac6718ec42e75e6db6909779c3ccee1548
Pierre Clerissi <pierre.clerissi@gmail.com>
2017-02-03 00:59:41 +0100

    Added rdisc6 to checkbin

--- checkbin.sh
+++ checkbin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for binDep in $(find -type f -exec sed -n '/.*\/bin\/.*\+/ { s,.*bin/\([[:alnum:]\-]\+\).*,\1,;p}' '{}' ';'  | sort -u) dhclient vconfig; do
+for binDep in $(find -type f -exec sed -n '/.*\/bin\/.*\+/ { s,.*bin/\([[:alnum:]\-]\+\).*,\1,;p}' '{}' ';'  | sort -u) dhclient vconfig rdisc6; do
 	binPath=$(/usr/bin/which $binDep)
 	[ -n "$binPath" ] && echo $binPath OK || { toFind=(${toFind[@]} "$binDep") ; echo $binDep KO; }
 done
